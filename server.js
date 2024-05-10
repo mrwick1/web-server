133de1a3b3ec6aea8c122caca7e334c41608f9b8
Arjun KR <arjunkrishnaraj123@gmail.com>
2024-05-10 23:54:44 +0530

    feat: added routes for the server

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-const http = require('http');
+const http = require("http");
 
-
-const hostname = 'localhost';
+const hostname = "localhost";
 
 const port = 3000;
 
-const server = http.createServer((req,res) => {
-    if (req.url === '/') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type','text/plain')
-        res.end('Hello from server')
-    }else if(req.url === '/login'){
-        res.statusCode = 200;
-        res.setHeader('Content-Type','text/plain')
-        res.end('Logged In')
-    }else{
-        res.statusCode = 404;
-        res.setHeader('Content-Type','text/plain')
-        res.end('404: Not found')
-    }
-})
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello from server");
+  } else if (req.url === "/login") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Logged In");
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("404: Not found");
+  }
+});
 
-
-server.listen(port,hostname,() => {
-    console.log(`server is up in http://${hostname}:${port}`)
-})
+server.listen(port, hostname, () => {
+  console.log(`server is up in http://${hostname}:${port}`);
+});
